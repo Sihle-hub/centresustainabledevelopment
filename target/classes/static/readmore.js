e8b3bf577e7c56b24528e992359246aff869f63b
Sihle <sihlesithole810@gmail.com>
2024-08-15 19:04:50 +0200

    csd datasource

---
+++ target/classes/static/readmore.js
@@ -0,0 +1,134 @@
+ /**ABOUT US */
+
+ document.getElementById('read-more-about').style.display = 'none';
+ document.getElementById('read-less-about-bn').style.display = 'none';
+ 
+
+ 
+ function openReadMoreAbout(){
+	
+	   document.getElementById('read-more-about').style.display = 'block';
+	   document.getElementById('read-more-about-bn').style.display = 'none';
+	   document.getElementById('read-less-about-bn').style.display = 'block';
+
+
+ }
+ 
+ function openReadLessAbout(){
+	
+	   document.getElementById('read-more-about').style.display = 'none';
+	   document.getElementById('read-more-about-bn').style.display = 'block';
+	   document.getElementById('read-less-about-bn').style.display = 'none';
+	
+ }
+ 
+ /*LOUIS*/
+ 
+ document.getElementById('read-more-louis').style.display = 'none';
+ document.getElementById('read-less-louis-bn').style.display = 'none';
+ 
+ 
+ function openReadMoreLouis(){
+	
+	   document.getElementById('read-more-louis').style.display = 'block';
+	   document.getElementById('read-more-louis-bn').style.display = 'none';
+	   document.getElementById('read-less-louis-bn').style.display = 'block';
+	   
+ }
+ 
+  function openReadLessLoius(){
+	
+	   document.getElementById('read-more-louis').style.display = 'none';
+	   document.getElementById('read-more-louis-bn').style.display = 'block';
+	   document.getElementById('read-less-louis-bn').style.display = 'none';
+	   
+ }
+ 
+ /*CHARLES BANDA*/ 
+ 
+ document.getElementById('read-more-charles').style.display = 'none';
+ document.getElementById('read-less-charles-bn').style.display = 'none';
+ 
+ 
+  function openReadMoreCharles(){
+	
+	   document.getElementById('read-more-charles').style.display = 'block';
+	   document.getElementById('read-more-charles-bn').style.display = 'none';
+	   document.getElementById('read-less-charles-bn').style.display = 'block';
+	   
+ }
+ 
+  function openReadLessLess(){
+	
+	   document.getElementById('read-more-charles').style.display = 'none';
+	   document.getElementById('read-more-charles-bn').style.display = 'block';
+	   document.getElementById('read-less-charles-bn').style.display = 'none';
+	   
+ }
+ 
+ 
+ /*HAMILTON VINCENT MLANDE */
+ 
+ 
+ document.getElementById('read-more-hamilton').style.display = 'none';
+ document.getElementById('read-less-hamilton-bn').style.display = 'none';
+ 
+ 
+  function openReadMoreHamilton(){
+	
+	   document.getElementById('read-more-hamilton').style.display = 'block';
+	   document.getElementById('read-more-hamilton-bn').style.display = 'none';
+	   document.getElementById('read-less-hamilton-bn').style.display = 'block';
+	   
+ }
+ 
+  function openReadLessHamilton(){
+	
+	   document.getElementById('read-more-hamilton').style.display = 'none';
+	   document.getElementById('read-more-hamilton-bn').style.display = 'block';
+	   document.getElementById('read-less-hamilton-bn').style.display = 'none';
+	   
+ }
+ 
+ /* DUDUZILE ZWANE */
+ 
+ 
+ document.getElementById('read-more-dudu').style.display = 'none';
+ document.getElementById('read-less-dudu-bn').style.display = 'none';
+ 
+ 
+  function openReadMoreDudu(){
+	
+	   document.getElementById('read-more-dudu').style.display = 'block';
+	   document.getElementById('read-more-dudu-bn').style.display = 'none';
+	   document.getElementById('read-less-dudu-bn').style.display = 'block';
+	   
+ }
+ 
+  function openReadLessDudu(){
+	
+	   document.getElementById('read-more-dudu').style.display = 'none';
+	   document.getElementById('read-more-dudu-bn').style.display = 'block';
+	   document.getElementById('read-less-dudu-bn').style.display = 'none';
+	   
+ }
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
